7e3cf9260e7ad1fbc45f216cea63f884e0b20212
Fen2187 <129371237+Fen2187@users.noreply.github.com>
2023-07-04 12:23:15 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,76 @@
 import discord
-import random as randint
+import random
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+  print(f'We have logged in as {client.user}')
+
+
 ##PREPROGRAMMED LINES
 @client.event
 async def on_message(ctx):
-    if ctx.author == client.user:
-        return
+  if ctx.author == client.user:
+    return
 
-    if ctx.content.startswith('hello'):
-        await ctx.channel.send('Hello!')
-    
-    if ctx.content.startswith('e'):
-        await ctx.channel.send('E')
-    
-    if ctx.content.startswith('E'):
-        await ctx.channel.send('E')
+  if ctx.content.startswith('hello'):
+    await ctx.channel.send('Hello!')
 
-    if ctx.content.startswith('ur fat'):
-        await ctx.channel.send("Your fat32 you can't store files over 4GB😎")
+  if ctx.content.startswith('e'):
+    await ctx.channel.send('E')
 
-    if ctx.content.startswith('$bank'):
-            await ctx.channel.send('You robbed money from the Fen bank!!!')
-            
-    if cycle.content.startwith('the discord mods'):
-        await ctx.channel.send('Nooooooooo!!!!!!!!')
-    
-    if ctx.content.startwith('android is better'):
-        await ctx.channel.send('Android sucks. oh you use Android LoL')
-        await ctx.channel.send('LoL')
-        await ctx.channel.send('LoL')
-        await ctx.channel.send('LoL')
-        
+  if ctx.content.startswith('E'):
+    await ctx.channel.send('E')
 
+  if ctx.content.startswith('/ur fat'):
+    await ctx.channel.send("Your fat32 you can't store files over 4GB😎")
 
+  if ctx.content.startswith('/bank'):
+    await ctx.channel.send('You robbed money from the Fen bank!!!')
 
+  if ctx.content.startswith('the discord mods'):
+    await ctx.channel.send('Nooooooooo!!!!!!!!')
 
-client.run('MTA5MzQ5MTQ1MDkxMTY3MDMwMg.GoTy-D.haY2rSw5ytrDDT44tQhU151bRJPIh1_7_B05Rk')
+  if ctx.content.startswith('android is better'):
+    await ctx.channel.send('Android sucks. oh you use Android LoL')
+    await ctx.channel.send('LoL')
+    await ctx.channel.send('LoL')
+    await ctx.channel.send('LoL')
+  #DnD dice help thing
+  if ctx.content.startswith('/DnD-help'):
+    await ctx.channel.send('What can Sir Fen AI do to help')
+
+  if ctx.content.startswith('/DnD-roll6'):
+    await ctx.channel.send(random.randint(1, 6))
+
+  if ctx.content.startswith('/DnD--roll6'):
+    await ctx.channel.send(random.randint(1, 6))
+    await ctx.channel.send(random.randint(1, 6))
+
+  if ctx.content.startswith('/DnD---roll6'):
+    await ctx.channel.send(random.randint(1, 6))
+    await ctx.channel.send(random.randint(1, 6))
+    await ctx.channel.send(random.randint(1, 6))
+
+  if ctx.content.startswith('/DnD----roll6'):
+    await ctx.channel.send(random.randint(1, 6))
+    await ctx.channel.send(random.randint(1, 6))
+    await ctx.channel.send(random.randint(1, 6))
+    await ctx.channel.send(random.randint(1, 6))
+
+  if ctx.content.startswith('/DnD-roll20'):
+    await ctx.channel.send(random.randint(1, 20))
+
+  if ctx.content.startswith('/DnD--roll20'):
+    await ctx.channel.send(random.randint(1, 20))
+    await ctx.channel.send(random.randint(1, 20))
+
+  if ctx.content.startswith('/DnD-roll80'):
+    await ctx.channel.send(random.randint(00, 80)('Just simplify the number, example 64 = 60 or 17 = 20'))
+client.run(
+  'MTA5MzQ5MTQ1MDkxMTY3MDMwMg.GC3B3B.zOQftu_Fb_Ieh5UspvPaOx0c01bwukbMm2a4MI')
